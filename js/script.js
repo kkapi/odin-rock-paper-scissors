8ddb6d36235b85aa85cd2959d977444939166d6a
kkapi <hZG2fik_j8@mail.ru>
2022-01-30 04:20:48 +0300

    done rock-paper-scissors

--- js/script.js
+++ js/script.js
@@ -7,19 +7,43 @@ function computerPlay() {
     let computerResponseString;
     switch(computerResponseDigit) {
         case 0:
-            computerResponseString = "Rock";
+            computerResponseString = "rock";
             break;
         case 1:
-            computerResponseString = "Paper";
+            computerResponseString = "paper";
             break;
         case 2:
-            computerResponseString = "Scissors";
+            computerResponseString = "scissors";
             break;
     }
     return computerResponseString;
 }
 
-for (let i = 0; i < 5; i++) {
-    console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log(`You Win! Rock beats Scissors`);
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log(`You Lose! Rock beats Scissors`);
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log(`You Win! Paper beats Rock`);
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log(`You Lose! Paper beats Rock`);
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log(`You Win! Scissors beats Paper`);
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log(`You Lose! Scissors beats Paper`);
+    } else {
+        console.log(`Draw`);
+    }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Make your choice (Rock, Paper, Scissors): ");
+        let computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+    }
+}
+
+game();
